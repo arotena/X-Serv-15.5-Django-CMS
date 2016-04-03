5cf518b5ada814426545f1406e708c9d61b9c95f
arotena <albertosss27@gmail.com>
2016-04-03 21:14:48 +0200

    realizado

--- cms/admin.py
+++ cms/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from models import Pages
 # Register your models here.
 
 from models import Pages
--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from models import Pages
 # Create your views here.
+
+def mostrar (request, key):
+    try:
+        valor = Pages.objects.get(id=key)
+        respuesta = "El valor es " + valor.page
+    except Pages.DoesNotExist:
+        respuesta = "Esta clave no existe"
+    return HttpResponse(respuesta)
+def add(request, key, valor):
+    nuevo = Pages(name=key,page=valor)
+    nuevo.save()
+    return HttpResponse("Nuevo elemento almacenado")
+def listar(request):
+    listado = Pages.objects.all()
+    respuesta = "<ol>"
+    for elemento in listado:
+        respuesta += "<li>" + "key: " + str(elemento.id)
+        respuesta += " name: " + str(elemento.name)
+    respuesta += "</ol>"
+    return HttpResponse(respuesta)
+def notFound(request):
+    return HttpResponse("Not Found: Argumentos invalidos")
